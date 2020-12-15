9d1ee13c281c2173e69eadba33794038f8bc7650
PageKallop <PageMarie78@gmail.com>
2020-12-15 11:27:13 -0600

    Set up UI Created currency array and updated UIViewPicker with it

---
+++ CryptoCoin/CoinManager.swift
@@ -0,0 +1,21 @@
+//
+//  CoinManager.swift
+//  CryptoCoin
+//
+//  Created by Page Kallop on 12/14/20.
+//
+
+import Foundation
+
+
+struct CoinManager {
+    
+    
+    
+    let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
+    
+    func getCoinPrice(for currency: String){
+        
+        print(currency)
+    }
+}
--- CryptoCoin/ViewController.swift
+++ CryptoCoin/ViewController.swift
@@ -7,13 +7,48 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
 
+    var coinManager = CoinManager()
+
+   
+    @IBOutlet weak var coinLabel: UILabel!
+    
+    @IBOutlet weak var currencyLabel: UILabel!
+    
+    @IBOutlet weak var currencyPicker: UIPickerView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
+        currencyPicker.dataSource = self
+        currencyPicker.delegate = self
     }
 
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        
+        
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+       
+        return coinManager.currencyArray.count
+        
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return coinManager.currencyArray[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        let currency = coinManager.currencyArray[row]
+        coinManager.getCoinPrice(for: currency)
+        
+        currencyLabel.text = coinManager.currencyArray[row] 
+    }
 
 }
 
